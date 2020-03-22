650e8022149d84480747333ca37330a4593baa7e
Melvin Kok <melvinkokxw@gmail.com>
2020-03-22 15:41:56 +0800

    added functions to generate number and compare guesses

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => Math.floor(Math.random()*10); //generates random integer between 0-9
+
+const compareGuesses = (humanGuess, computerGuess, actualTarget) => {
+  if(Math.abs(humanGuess-actualTarget)<=Math.abs(computerGuess-actualTarget)) {
+    return true;
+  } else {
+    return false;
+  }
+};
