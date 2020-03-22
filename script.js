ef0a354562e7ae70d417876650e85e0c1ea65258
Melvin Kok <melvinkokxw@gmail.com>
2020-03-22 15:48:47 +0800

    added function to update score and advance round

--- script.js
+++ script.js
@@ -5,10 +5,19 @@ let currentRoundNumber = 1;
 // Write your code below:
 const generateTarget = () => Math.floor(Math.random()*10); //generates random integer between 0-9
 
-const compareGuesses = (humanGuess, computerGuess, actualTarget) => {
-  if(Math.abs(humanGuess-actualTarget)<=Math.abs(computerGuess-actualTarget)) {
-    return true;
+//compare the differences between the human's guess and computer's guess to determine the winner
+const compareGuesses = (humanGuess, computerGuess, actualTarget) => Math.abs(humanGuess-actualTarget)<=Math.abs(computerGuess-actualTarget);
+
+//update the score according to the winner of the round
+const updateScore = winner => {
+  if (winner==='human') {
+    humanScore++;
   } else {
-    return false;
+    computerScore++;
   }
+};
+
+
+const advanceRound = () => {
+  currentRoundNumber++;
 };
